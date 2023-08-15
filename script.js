a90ccdaf5b105a5adeba7ff02a924dd07614b935
Nogiback <peter.toan.do@gmail.com>
2023-08-15 13:07:30 -0400

    Added password and confirm password validation in script. Minor styling changes. Minor refactoring

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const email = document.querySelector('#email');
 const emailError = document.querySelector('#email-error');
 const phoneNumber = document.querySelector('#phone-number');
 const phoneNumberError = document.querySelector('#tel-error');
+const password = document.querySelector('#password');
+const passwordError = document.querySelector('#password-error');
+const confirmPassword = document.querySelector('#confirm-password');
+const confirmPasswordError = document.querySelector('#confirm-password-error');
 
 firstName.addEventListener('input', function(event) {
   if(firstName.value === '') {
@@ -36,5 +40,32 @@ phoneNumber.addEventListener('input', function(event) {
     phoneNumberError.textContent = '*Please enter a 10 digit phone number';
   } else {
     phoneNumberError.textContent = '';
+  }
+});
+
+password.addEventListener('input', function(event) {
+  let input = password.value;
+  const upperRegex = /[A-Z]/;
+  const numberRegex = /[0-9]/;
+
+  if (input.length > 7) {
+    if (!upperRegex.test(input) || !numberRegex.test(input)) {
+      passwordError.textContent = '*Password must contain at least one uppercase letter and one digit';
+    } else {
+      passwordError.textContent = '';
+    }
+  } else {
+    passwordError.textContent = '*Password must be at least 8 characters long';
+  }
+
+});
+
+confirmPassword.addEventListener('input', function(event) {
+  if(confirmPassword.value === '') {
+    confirmPasswordError.textContent = '*Please confirm your password';
+  } else if (confirmPassword.value !== password.value) {
+    confirmPasswordError.textContent = '*Passwords do not match!';
+  } else {
+    confirmPasswordError.textContent = '';
   }
 });
